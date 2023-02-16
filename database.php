1c37a5d0c39a0b2a908ba710d9da7ce8ce185404
MattSux <89954184+MattSux@users.noreply.github.com>
2023-02-16 16:19:38 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,16 +1,15 @@
 <?php
 
 // Connect to the MySQL database using PDO
-$dsn = "mysql:host=localhost;dbname=todolist";
-$username = "root";
-$password = "7077Mgh257!!!!!";
-
+$dsn = "mysql:host=localhost;dbname=todolist"; // the DSN string for the MySQL database
+$username = "root"; // the username for the MySQL database
+$password = "7077Mgh257!!!!!"; // the password for the MySQL database
 
 try {
-    $conn = new PDO($dsn, $username, $password);
+    $conn = new PDO($dsn, $username, $password); // create a new PDO connection using the DSN, username, and password
 }
 catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage(); // if the connection fails, display an error message
 }
 
 ?>
